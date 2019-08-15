b0f37474c7d7ab8ce085f1009e35af802ab32f3d
thr3a <thr3a@example.com>
2019-08-15 11:47:41 +0900

    fix

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,21 @@
 <template>
   <section class="section">
-    <div class="header-container">
+    <div class="header-container" id="index">
       <div class="columns is-multiline is-mobile" id="header">
         <div class="column is-6-mobile" v-for="(name, index) in menu" :key="index">
-          <h1 class="subtitle is-3 has-text-centered">
-            <nuxt-link v-scroll-to="{ el: `#${name.toLowerCase()}`, offset: -headerHeight}" to="#">{{ name }}</nuxt-link>
+          <h1 class="subtitle is-4 has-text-centered">
+          <div v-if="index == 0">
+            <nuxt-link
+              v-scroll-to="{ el: `#${name.toLowerCase()}`}"
+              to="#"
+            >{{ name }}</nuxt-link>
+          </div>
+          <div v-else>
+            <nuxt-link
+              v-scroll-to="{ el: `#${name.toLowerCase()}`, offset: -headerHeight}"
+              to="#"
+            >{{ name }}</nuxt-link>
+          </div>
           </h1>
         </div>
       </div>
@@ -16,13 +27,13 @@
 export default {
   data: function() {
     return {
-      menu: ['Profile', 'Works', 'Links'],
+      menu: ['Index', 'Profile', 'Works', 'Links'],
       headerHeight: 0
     }
   },
   mounted: function() {
     // ヘッダーの高さを取得してスクロール時のoffsetに入れることで、コンテンツが隠れないように
-    this.headerHeight = document.getElementById('header').offsetHeight;
+    this.headerHeight = document.getElementById('header').offsetHeight
   }
 }
 </script>
@@ -32,7 +43,7 @@ export default {
   top: 0;
   left: 0;
   position: fixed;
-  width:100%;
+  width: 100%;
   z-index: 334;
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
   background-color: #ffffff;
