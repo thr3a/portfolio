47f158a8e37c02ded5479c6a925931a0be693fbc
thr3a <thr3a@example.com>
2022-08-14 00:14:49 +0900

    update:

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
+  experimental: {
+    images: {
+      unoptimized: true
+    }
+  },
+  images: {
+    unoptimized: true
+  }
 }
-
 module.exports = nextConfig
