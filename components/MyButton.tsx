cd71d32d0421585c3212726e523793b58e41bfb5
thr3a <thr3a@example.com>
2022-08-20 13:36:05 +0900

    add: jsonでSNSリンク作成

---
+++ components/MyButton.tsx
@@ -0,0 +1,49 @@
+import { Button, Text } from '@mantine/core';
+import { IconBrandTwitter, IconEdit, IconExternalLink, IconBrandGithub, TablerIcon } from '@tabler/icons';
+
+type StringKeyObject = {
+  [key: string]: TablerIcon;
+}
+
+const iconMap: StringKeyObject = {
+  twitter: IconBrandTwitter,
+  blog: IconEdit,
+  external: IconExternalLink,
+  github: IconBrandGithub
+};
+
+type Props = {
+  url: string,
+  name: string,
+  color: string,
+  icon: keyof typeof iconMap
+}
+
+export default function MyButton(props: Props) {
+  const Component = iconMap[props.icon];
+  console.log(props);
+  
+  return (
+    <div style={{ width: 200 }}>
+      <Button
+        fullWidth
+        size="lg"
+        color={props.color}
+        component="a"
+        target="_blank"
+        radius="xs"
+        href={props.url}
+        styles={(theme) => ({
+          root: {
+            marginTop: 5,
+            marginBottom: 5
+          }
+        })}
+        leftIcon={<Component />}
+      >
+        <Text align="left">{props.name}</Text>
+      </Button>
+    </div>
+
+  );
+}
--- pages/page2.tsx
+++ pages/page2.tsx
@@ -1,14 +1,44 @@
-import type { NextPage } from 'next';
-import { Center } from '@mantine/core';
+import { GetServerSideProps } from "next";
+import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+import { IconExternalLink } from '@tabler/icons';
+import { IconBrandTwitter } from '@tabler/icons';
+import MyButton from '../components/MyButton';
 
-const MyPage: NextPage = () => {
+// propsの型を定義する
+type Props = {
+  title?: string;
+  num?: number;
+};
+
+// ページコンポーネントを定義する
+const TestPage = (props: Props) => {
   return (
     <div>
-      <Center style={{ width: 400, height: 200 }}>
-        <div>All elements inside Center are centered</div>
-      </Center>
+      <div>
+        <MyButton url="https://twitter.com/amanekey" name="@amanekey" color="blue" icon="twitter"></MyButton>
+      </div>
+      <div>
+        <MyButton url="https://thr3a.hatenablog.com/" name="Blog" color="orange" icon="blog"></MyButton>
+      </div>
+      <div>
+        <MyButton url="" name="zenn" color="orange" icon="external"></MyButton>
+      </div>
     </div>
   );
 };
 
-export default MyPage;
+export default TestPage;
+
+// サーバサイドで実行する処理(getServerSideProps)を定義する
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // APIやDBからのデータ取得処理などを記載
+
+  const props: Props = {
+    title: "testtitle",
+    num: 123,
+  };
+
+  return {
+    props: props,
+  };
+};
