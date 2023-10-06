9af40aa4ae104fadd172f9e061d5c2da2efefa12
thr3a <thr3a@example.com>
2023-10-07 06:48:06 +0900

    add: sitemap生成

---
+++ src/scripts/sitemap.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import dayjs from 'dayjs';
+import { WorkData } from '../data/works';
+
+function createSitemap (urls: string[]): string {
+  const now = dayjs();
+  const iso8601 = now.format('YYYY-MM-DDTHH:mm:ssZ');
+
+  let sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n';
+  sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+
+  for (const url of urls) {
+    sitemap += '<url>\n';
+    sitemap += `<loc>${url}</loc>\n`;
+    sitemap += `<lastmod>${iso8601}</lastmod>\n`;
+    sitemap += '</url>\n';
+  }
+
+  sitemap += '</urlset>';
+  return sitemap;
+}
+
+const testSitemap = createSitemap(WorkData.map(x => x.url));
+fs.writeFileSync('dist/sitemap.xml', testSitemap);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
+import { WorkData } from './src/data/works';
 
-// https://vitejs.dev/config/
+const urls = WorkData.map(x => x.url);
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    Sitemap({ dynamicRoutes: urls })
+  ]
 });
