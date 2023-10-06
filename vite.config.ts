fd157d5b28596376d2c98a2792d482dff27f7125
thr3a <thr3a@example.com>
2023-10-07 06:50:31 +0900

    fix:

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Sitemap from 'vite-plugin-sitemap';
-import { WorkData } from './src/data/works';
-
-const urls = WorkData.map(x => x.url);
 
 export default defineConfig({
-  plugins: [
-    react(),
-    Sitemap({ dynamicRoutes: urls })
-  ]
+  plugins: [react()]
 });
