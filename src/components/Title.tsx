175336e02a7022828923a2a7b3f0c792efedb116
thr3a <thr3a@example.com>
2025-05-24 00:54:46 +0900

    fix: タイトルダークモードで見えづらくなっているのを修正

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,13 +1,14 @@
-import { Text, useMantineTheme } from '@mantine/core';
+import { Text, useMantineColorScheme, useMantineTheme } from '@mantine/core';
 
 export const Title = ({ title }: { title: string }) => {
   const theme = useMantineTheme();
+  const { colorScheme } = useMantineColorScheme();
 
   return (
     <>
       <Text mt='xl' mb='md' fz='h1' c={theme.primaryColor}>
         ##{' '}
-        <Text component='span' fw={'bold'} c={'dark'} inherit>
+        <Text component='span' fw={'bold'} c={colorScheme === 'dark' ? theme.colors.gray[0] : theme.black} inherit>
           {title}
         </Text>
       </Text>
