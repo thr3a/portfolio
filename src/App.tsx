79a1cb05394807eee47a0411ede9e427009a0268
thr3a <thr3a@example.com>
2025-05-24 00:54:02 +0900

    clean: SNS削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { ColorSchemeScript, Container, MantineProvider, Stack } from '@mantine/core';
+import { ColorSchemeScript, Container, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { useState } from 'react';
 import { Header } from './components/Header';
 import { Profile } from './components/Profile';
-import { SnsLink } from './components/SnsLink';
 import { Title as MyTitle } from './components/Title';
 import { WorkGroup } from './components/WorkGroup';
-import { SnsData } from './data/sns';
 import { theme } from './theme';
 
 export default function App() {
@@ -18,12 +16,6 @@ export default function App() {
         <Header />
         <MyTitle title='Profile' />
         <Profile />
-        <MyTitle title='Navigation' />
-        <Stack gap={'xs'}>
-          {SnsData.map((props, index) => (
-            <SnsLink key={index} url={props.url} name={props.name} color={props.color} icon={props.icon} />
-          ))}
-        </Stack>
         <MyTitle title='Works' />
         <WorkGroup workGroup={workGroup} setworkGroup={setworkGroup} />
       </Container>
