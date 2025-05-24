154c5f2b2a76b11a4feb94a30952c4ad901d61eb
thr3a <thr3a@example.com>
2025-05-24 18:35:27 +0900

    update:

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFound from './NotFound';
+import Make12Page from './make12/Page';
 import Password from './password/Page';
 import Portfolio from './portfolio/Page';
 import Template from './template/Page';
@@ -12,6 +13,7 @@ export default function App() {
         <Route path='/' element={<Portfolio />} />
         <Route path='/template' element={<Template />} />
         <Route path='/password' element={<Password />} />
+        <Route path='/make12' element={<Make12Page />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
