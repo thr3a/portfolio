e21e8d3adc2e37625cbd7c00552c64f0312b63d7
thr3a <thr3a@example.com>
2026-03-01 01:37:33 +0900

    update: 色変更

--- src/portfolio/Work.tsx
+++ src/portfolio/Work.tsx
@@ -5,13 +5,13 @@ import { text } from './Work.css';
 export const Work = ({ title, description, url, emoji }: WorkProps) => {
   return (
     <Paper
-      bd='1px solid var(--mantine-primary-color-5)'
+      bd='1px solid pink.6'
       p='md'
       component='a'
       radius={0}
       target='_blank'
       href={url}
-      c='var(--mantine-primary-color-8)'
+      c='green.9'
       style={{ textDecoration: 'none', display: 'block' }}
     >
       <Stack gap='sm'>
